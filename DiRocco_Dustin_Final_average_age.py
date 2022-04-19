d4d00b05d44c21c186a6a1ae84a8ace7dbf61bde
Dustin8226 <104039351+Dustin8226@users.noreply.github.com>
2022-04-19 19:09:33 -0400

    Add files via upload

---
+++ DiRocco_Dustin_Final_average_age.py
@@ -0,0 +1,7 @@
+# Program name: DiRocco_Dustin_Final
+# Author: Dustin DiRocco
+# Date: 4/12/22
+# Summary: Program will read a file that lists the ages, add them together and get the average. 
+# Variables
+
+def AvAge()
