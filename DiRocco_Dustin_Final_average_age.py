ea8529fe58815112a634bde6819eedc715f73423
Dustin8226 <104039351+Dustin8226@users.noreply.github.com>
2022-04-19 20:38:29 -0400

    Delete DiRocco_Dustin_Final_average_age.py

--- DiRocco_Dustin_Final_average_age.py
+++
@@ -1,7 +0,0 @@
-# Program name: DiRocco_Dustin_Final
-# Author: Dustin DiRocco
-# Date: 4/12/22
-# Summary: Program will read a file that lists the ages, add them together and get the average. 
-# Variables
-
-def AvAge()
