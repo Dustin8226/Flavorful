cf5b48f0e8641920df1c4bc342be830534b6f97b
Dustin8226 <104039351+Dustin8226@users.noreply.github.com>
2022-04-19 20:38:05 -0400

    Delete DiRocco_Dustin_Final.py

--- DiRocco_Dustin_Final.py
+++
@@ -1,7 +0,0 @@
-# Program name: DiRocco_Dustin_Final
-# Author: Dustin DiRocco
-# Date: 4/12/22
-# Summary: Program will open the main function, import information from 2 seperate python files, and list the faviortes from the survey plus the average age.
-# Variables
-
-def main ()
